7b3bdb36c92353fe1dcc6cddc0d48cc5e7e07907
Araba Taylor <araba.taylor20@gmail.com>
2017-08-30 02:31:46 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,7 +1,7 @@
 def position_taken?(board,index)
 if board[index] == " " || board[index] == "" || "nil"
-  position_taken? =false
+  false
 elsif board[index] == "X" || "O"
-  position_taken? =true
+  true
 end
 end
